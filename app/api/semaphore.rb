6da3bddcc068d13fbfd3dff79c7be03dff83295f
Matias Leidemer <matiasleidemer@gmail.com>
2014-05-05 22:45:21 -0300

    Added AFMotion to semaphore login method

--- app/api/semaphore.rb
+++ app/api/semaphore.rb
@@ -3,7 +3,7 @@ class Semaphore
   PROJECTS_URL = API_URL + 'projects.json'
 
   def self.login(token, &block)
-    BW::HTTP.get(PROJECTS_URL + "?auth_token=#{token}") do |response|
+    AFMotion::JSON.get PROJECTS_URL, :auth_token => token do |response|
       block.call response
     end
   end
