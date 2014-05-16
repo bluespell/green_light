2a6befc7d4ba44581960504107073708dada6000
Fernando Fernandes <mfernandes.fernando@gmail.com>
2014-05-16 01:31:01 -0300

    Added motion-reveal gem and fixed a serious leaking issue:
     - https://github.com/DiogoAndre/motion-reveal
     - An example (look how awesome this is): http://cl.ly/image/411v3t0d0X3k
     - Fixed the issue where the labels were reappearing. This table was leaking so hard that it gave me instant eye cancer.
    
    !!! Although I fixed it, PaperFold won't work as we would like it to. It is *almost* impossible to keep scrolling the cells w/o some type of sorcery. It is not worth it. Well, back to lab... !!!

--- app/services/project_cell.rb
+++ app/services/project_cell.rb
@@ -1,31 +1,29 @@
 class ProjectCell < UITableViewCell
 
+  attr_accessor :background_color, :paper_fold, :left_content, :favorite_icon, :center_content
+
   def configure(project)
-    background_color = UIView.alloc.init
-    background_color.setBackgroundColor(project.status_color[:foreground])
-    self.setSelectedBackgroundView(background_color)
+    @background_color = UIView.alloc.init unless @backgroundColor
+    @background_color.setBackgroundColor(project.status_color[:foreground])
+    self.setSelectedBackgroundView(@background_color)
 
-    paper_fold = PaperFoldView.alloc.initWithFrame([[0, 0],[self.frame.size.width, self.frame.size.height]])
-    paper_fold.setEnableRightFoldDragging(false)
+    @paper_fold = PaperFoldView.alloc.initWithFrame([[0, 0],[self.frame.size.width, self.frame.size.height]]) unless @paper_fold
 
-    left_content = UIView.alloc.initWithFrame([[0, 0],[60, self.frame.size.height]])
-    left_content.setAutoresizingMask(UIViewAutoresizingFlexibleHeight|UIViewAutoresizingFlexibleWidth)
-    left_content.setBackgroundColor(project.status_color[:background])
+    @left_content = UIView.alloc.initWithFrame([[0, 0],[60, self.frame.size.height]]) unless @left_content
+    @left_content.setAutoresizingMask(UIViewAutoresizingFlexibleHeight|UIViewAutoresizingFlexibleWidth)
+    @left_content.setBackgroundColor(project.status_color[:background])
 
-    if (project.favorite)
-      favorite_icon = UIImageView.alloc.initWithImage(UIImage.imageNamed('star-40-red'))
-    else
-      favorite_icon = UIImageView.alloc.initWithImage(UIImage.imageNamed('star-40-green'))
-    end
-    favorite_icon.center = left_content.center
+    @favorite_icon = UIImageView.alloc.initWithImage(UIImage.imageNamed('star-40-green')) unless @favorite_icon
+    @favorite_icon.setImage(UIImage.imageNamed('star-40-red')) if (project.favorite)
+    @favorite_icon.center = @left_content.center
 
-    left_content.addSubview(favorite_icon)
-    paper_fold.setLeftFoldContentView(left_content)
+    @left_content.addSubview(favorite_icon)
+    @paper_fold.setLeftFoldContentView(@left_content)
 
-    center_content = ProjectCellView.alloc.initWithFrame([[0, 0],[self.frame.size.width, self.frame.size.height]])
-    center_content.configure(project)
-    paper_fold.setCenterContentView(center_content)
+    @center_content = ProjectCellView.alloc.initWithFrame([[0, 0],[self.frame.size.width, self.frame.size.height]]) unless @center_content
+    @center_content.configure(project)
+    @paper_fold.setCenterContentView(@center_content)
 
-    self.addSubview(paper_fold)
+    self.addSubview(@paper_fold)
   end
 end
--- app/services/project_cell_view.rb
+++ app/services/project_cell_view.rb
@@ -1,7 +1,7 @@
 class ProjectCellView < UIView
   include TimeHelper
 
-  attr_accessor :project
+  attr_accessor :project, :project_name, :project_details
 
   def configure(project)
     @project = project
@@ -9,18 +9,18 @@ class ProjectCellView < UIView
     self.setBackgroundColor(@project.status_color[:background])
 
     # Project Name
-    project_name = UILabel.alloc.initWithFrame([[20, 7],[222, 21]])
-    project_name.font = UIFont.fontWithName("HelveticaNeue-Bold", size:16)
-    project_name.setTextColor('4A4A4A'.to_color)
-    project_name.text = @project.name
-    self.addSubview(project_name)
+    @project_name = UILabel.alloc.initWithFrame([[20, 7],[222, 21]]) unless @project_name
+    @project_name.font = UIFont.fontWithName("HelveticaNeue-Bold", size:16)
+    @project_name.setTextColor('4A4A4A'.to_color)
+    @project_name.text = @project.name
+    self.addSubview(@project_name)
 
     # Project Details
-    project_details = UILabel.alloc.initWithFrame([[20, 32],[222, 21]])
-    project_details.font = UIFont.fontWithName("HelveticaNeue", size: 14)
-    project_details.setTextColor('4A4A4A'.to_color)
-    project_details.text = detail_text
-    self.addSubview(project_details)
+    @project_details = UILabel.alloc.initWithFrame([[20, 32],[222, 21]]) unless @project_details
+    @project_details.font = UIFont.fontWithName("HelveticaNeue", size: 14)
+    @project_details.setTextColor('4A4A4A'.to_color)
+    @project_details.text = detail_text
+    self.addSubview(@project_details)
   end
 
   private
