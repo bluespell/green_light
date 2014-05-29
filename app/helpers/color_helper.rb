2f9f88d2762332a2bf55e4fcef2d39347bcf45b1
Fernando Fernandes <mfernandes.fernando@gmail.com>
2014-05-28 21:14:08 -0300

    Refactored ColorHelper to reuse code.

--- app/helpers/color_helper.rb
+++ app/helpers/color_helper.rb
@@ -1,17 +1,24 @@
 module ColorHelper
-  def failed_colors
-    { :light => "F5D9C9".to_color, :medium => "F2F2E9".to_color, :strong => "AF3A00".to_color }
+  def passed_colors
+    { :light => "E6F5C8".to_color, :medium => "F2F2E9".to_color, :strong => "8EB43F".to_color }
   end
 
   def pending_colors
     { :light => "DCEFF9".to_color, :medium => "F2F2E9".to_color, :strong => "A2BCCD".to_color }
   end
 
-  def passed_colors
-    { :light => "E6F5C8".to_color, :medium => "F2F2E9".to_color, :strong => "8EB43F".to_color }
+  def stopped_colors
+    stopped_failed_colors
   end
 
-  def stopped_colors
+  def failed_colors
+    stopped_failed_colors
+  end
+
+  private
+
+  # Stopped and Failed are both same 'redish' colors
+  def stopped_failed_colors
     { :light => "F5D9C9".to_color, :medium => "F2F2E9".to_color, :strong => "AF3A00".to_color }
   end
 end
