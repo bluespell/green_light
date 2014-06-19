baa69751f05899af5fcaa0696584c7ae8032be92
Fernando Fernandes <mfernandes.fernando@gmail.com>
2014-06-19 14:44:18 -0300

    Set Refresh Control behavior via its Helper.

--- app/controllers/builds_controller.rb
+++ app/controllers/builds_controller.rb
@@ -1,7 +1,17 @@
 class BuildsController < UITableViewController
 
+  def viewDidAppear(animated)
+    RefreshControlHelper.configure(refreshControl, self, :refresh_builds)
+  end
+
   # Pops the current view and gets back to the previous one
   def back(sender)
     self.navigationController.popViewControllerAnimated(true)
+  end
+
+  private
+
+  def refresh_builds
+    RefreshControlHelper.trigger_refresh(refreshControl, cdq, tableView)
   end
 end
