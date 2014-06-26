23db23cf0617d2bef4b02d61a8d65de9a5654819
Matias Leidemer <matiasleidemer@gmail.com>
2014-06-26 00:32:06 -0300

    Fixed Project last_build_cache attribute setting
    This attribute is responsible for the correct ordering in the
    tableView. It's a temporary fix, since we need to use a
    before save callback or something

--- app/commands/authentication_command.rb
+++ app/commands/authentication_command.rb
@@ -1,12 +1,18 @@
 class AuthenticationCommand
   def self.run(token, &block)
     SVProgressHUD.appearance.setHudBackgroundColor("F2F2E9".to_color)
-    SVProgressHUD.showWithStatus("Loading", maskType:SVProgressHUDMaskTypeGradient)
+    SVProgressHUD.showWithStatus("Loading", maskType: SVProgressHUDMaskTypeGradient)
 
     RemoteManager.object_manager.getObjectsAtPath(
       'projects.json',
       parameters: { auth_token: token },
       success: ->(operation, mapping_result) {
+
+        # FIXME: this needs to go to a model `before_save` callback
+        mapping_result.array.each do |project|
+          project.last_build_cache = project.last_build
+        end
+
         SVProgressHUD.showSuccessWithStatus "Success"
 
         block.call
